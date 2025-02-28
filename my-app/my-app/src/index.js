0251a575bb88c99f3b9329ce6faea0a4d0d07ab9
Oliwia000 <152185830+Oliwia000@users.noreply.github.com>
2025-02-28 18:40:39 +0100

    Update index.js

--- my-app/my-app/src/index.js
+++ my-app/my-app/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client"; 
-import Aplikacja from "./Aplikacja"; 
+import Aplikacja from "./App"; 
 
 const root = ReactDOM.createRoot(document.getElementById("korzen"));
 root.render(<Aplikacja />);
